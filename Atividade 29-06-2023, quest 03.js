35177c781f0754b0840de3262dbdb56deb92cabd
Layza Nicolle <129307127+LayzaNicolle@users.noreply.github.com>
2023-06-29 17:57:25 -0300

    Atividade 29/06/2023, questão 03

---
+++ Atividade 29-06-2023, quest 03.js
@@ -0,0 +1,30 @@
+//Quest 03
+
+var x = 10
+var y = 5
+x += y;
+console.log("Resultado da soma entre x e y é igual a:", x);
+
+
+// x = x * y (x = 10, y = 5)
+x = 10
+x *= y;
+console.log("Resultado da multiplicação entre x e y é igual a:", x);
+
+// x %= y
+x = 10
+x %= y;
+console.log("Resultado do resto da divisão entre x e y é igual a:", x);
+
+// x = x / y 
+x = 10
+x /= y;
+console.log("Resultado da divisão entre x e y é igual a:", x);
+
+
+// x = x - y (x = 10, y = 10)
+x = 10
+x -= y;
+console.log("Resultado da subtração entre x e y é igual a:", x);
+
+
