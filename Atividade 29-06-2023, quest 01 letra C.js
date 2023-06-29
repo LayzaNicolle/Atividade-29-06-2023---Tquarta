2aeade2947f590c86bad564b74e76687c0f7ce24
Layza Nicolle <129307127+LayzaNicolle@users.noreply.github.com>
2023-06-29 16:40:15 -0300

    Atividade 29/06/2023 JS, quest 01 letra C

---
+++ Atividade 29-06-2023, quest 01 letra C.js
@@ -0,0 +1,12 @@
+//Questão 01, letra c
+
+var x = "7"
+var y = 7
+
+let resultado = (x < 10 && y >= 7)  
+console.log(resultado)
+
+console.log ( 7 == "7"); 
+console.log ( 7 === "7"); 
+console.log ( "7" === "7"); 
+console.log ( "7" == "7"); 
