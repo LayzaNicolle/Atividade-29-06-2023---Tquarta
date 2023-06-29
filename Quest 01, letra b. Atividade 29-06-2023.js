65166502492b0378df0c3ea207f446ee503cc11e
Layza Nicolle <129307127+LayzaNicolle@users.noreply.github.com>
2023-06-29 16:24:33 -0300

    Ativ 29/06/2023. Questão 01, letra b

---
+++ Quest 01, letra b. Atividade 29-06-2023.js
@@ -0,0 +1,13 @@
+//Questão 01, letra b
+
+var x = 6
+var y = "6"
+
+var result = (x >= 6 && y < "6")
+console.log (result)
+
+
+console.log ( 6 != "6")
+console.log ( 6 !== "6")
+console.log ( "6" !== 6)
+console.log ( "6" !== "60")
