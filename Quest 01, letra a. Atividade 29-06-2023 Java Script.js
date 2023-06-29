cadb056e39cad12ca01801988aec27e3593f85e0
Layza Nicolle <129307127+LayzaNicolle@users.noreply.github.com>
2023-06-29 16:15:14 -0300

    Ativ 29/06/2023 Java Script. Quest 01, letra A

---
+++ Quest 01, letra a. Atividade 29-06-2023 Java Script.js
@@ -0,0 +1,15 @@
+//Questão 01, letra A
+
+var x = 5;
+var y = 8;
+
+var result = (x >= 7 && y < 10 )
+console.log (result)
+
+result = (x == '5' || y == '8')
+console.log (result)
+
+console.log (x === "5")
+console.log (y === "8")
+
+
